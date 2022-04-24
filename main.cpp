2d3bd8238d3f6b7ca77045d2a704ea4de3d44523
Bharat Bhushan <bbhushan@uoguelph.ca>
2022-04-24 17:42:49 -0400

    Added main.cpp and makefile

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    
+    int num;
+
+    std::cout << "Enter the number that you like the most between 1 and 100:";
+    std::cin >> num;
+    std::cout << "Amazing!! That is the number that I like the most too!" << std::endl;
+    std::cout << "No really!!, " << num << " is the number that I like the most!" << std::endl;
+
+    return 0; 
+}
---
+++ makefile
@@ -0,0 +1,14 @@
+CC = g++
+
+CFLAGS = -Wall -pedantic -std=c++17
+
+all: main
+
+main: main.obj
+
+	$(CC) $(CFLAGS) main.obj -o main
+
+main.obj: main.cpp
+
+	$(CC) $(CFLAGS) -c main.cpp -o main.obj
+
